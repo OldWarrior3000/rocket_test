c0b33f90b6068e6e26aa8b1199eb7c45b09c9414
Dietmar Ruef <dietmar.ruef@lemax.net>
2022-01-16 17:27:47 +0100

    Simple example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+
+#[cfg(test)] mod tests;
+
+#[get("/hello/<name>/<age>")]
+fn hello(name: String, age: u8) -> String {
+    format!("Hello, {} year old named {}!", age, name)
+}
+
+#[get("/hello/<name>")]
+fn hi(name: String) -> String {
+    name
+}
+
+#[get("/hello2/<name>/<last_name>")]
+fn hi2(name: String, last_name: String) -> String {
+    format!("Name: {}, last name: {}", name, last_name)
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite().mount("/", routes![hello, hi,hi2]).launch();
 }
